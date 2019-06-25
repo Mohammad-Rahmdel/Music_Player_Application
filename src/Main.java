a1bfb2152ec87a2dc52e7f120fdabc5a9d6a71ca
Mohammad-Rahmdel <unknown13766@yahoo.com>
2019-06-26 00:00:34 +0430

    sleep mode function

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 public class Main {
 
+    public static void sleep(int time){
+        try {
+            Thread thread = new Thread();
+            thread.sleep(time);
+            System.exit(0);
+        } catch (Exception e) {}
+    }
     public static void main(String[] args) {
 
         GUI gui = new GUI();
         gui.show();
 
+        // sleep(10000);
     }
 }
